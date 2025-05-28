66d2ebe9a275a762a3ef2707a8d488398ac1389b
Vadim Antonyuk <vadim.antonyuk1@gmail.comgit commit -m initgit config --global user.email vadim.antonyuk1@gmail.com>
2025-05-28 15:33:52 +0300

    refactor(gulp): покращено структуру тасків для SCSS, JS, зображень і шрифтів

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@ const { src, dest, watch, parallel, series } = require('gulp');
 const sass = require('gulp-dart-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-(fileinclude = require('gulp-file-include')),
-	(del = require('del')),
-	(htmlmin = require('gulp-htmlmin')),
-	(cfg = require('./package.json').config),
-	(csso = require('gulp-csso')),
-	(concat = require('gulp-concat')),
-	(autoprefixer = require('gulp-autoprefixer')),
-	(browserSync = require('browser-sync').create()),
-	(terser = require('gulp-terser')),
-	(browserslist = ['> 1%, last 3 versions, not dead']);
+const fileinclude = require('gulp-file-include');
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const cfg = require('./package.json').config;
+const csso = require('gulp-csso');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const terser = require('gulp-terser');
+const browserslist = ['> 1%, last 3 versions, not dead'];
 
 function html() {
-	return src([cfg.srcDir + '/*.html'])
+	return src(cfg.srcDir + '/*.html')
 		.pipe(
 			fileinclude({
 				prefix: '@@',
@@ -25,8 +25,9 @@ function html() {
 		.pipe(dest(cfg.outputDir))
 		.pipe(browserSync.stream({ once: true }));
 }
+
 function htmlMin() {
-	return src([cfg.srcDir + '/*.html'])
+	return src(cfg.srcDir + '/*.html')
 		.pipe(
 			fileinclude({
 				prefix: '@@',
@@ -40,24 +41,23 @@ function htmlMin() {
 async function fonts() {
 	const ttf2woff2 = (await import('gulp-ttf2woff2')).default;
 
-	// Copy the original fonts
-	src('src/fonts/**/*', { encoding: false }).pipe(dest('app/fonts'));
+	// Копіюємо оригінальні шрифти
+	src(cfg.srcDir + '/fonts/**/*', { encoding: false }).pipe(dest(cfg.outputDir + '/fonts'));
 
-	// Convert only .ttf to .woff2
-	return src('src/fonts/**/*.ttf', { encoding: false })
+	// Конвертуємо .ttf у .woff2
+	return src(cfg.srcDir + '/fonts/**/*.ttf', { encoding: false })
 		.pipe(plumber({ errorHandler: notify.onError('Font Error: <%= error.message %>') }))
 		.pipe(ttf2woff2())
-		.pipe(dest('app/fonts'));
+		.pipe(dest(cfg.outputDir + '/fonts'));
 }
 
-
 function styles() {
-	return src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
+	return src(cfg.srcDir + '/scss/**/*.{scss,sass}', { sourcemaps: true })
 		.pipe(plumber({ errorHandler: notify.onError('Sass Error: <%= error.message %>') }))
 		.pipe(
 			sass({
-				outputStyle: 'expanded', // expanded/compressed
-				silenceDeprecations: ['legacy-js-api'], // Disable old API warnings
+				outputStyle: 'expanded',
+				silenceDeprecations: ['legacy-js-api'],
 			}).on('error', sass.logError),
 		)
 		.pipe(
@@ -65,42 +65,39 @@ function styles() {
 				overrideBrowserslist: browserslist,
 			}),
 		)
-
-		.pipe(dest(cfg.outputDir + 'css', { sourcemaps: '.' }))
+		.pipe(dest(cfg.outputDir + '/css', { sourcemaps: '.' }))
 		.pipe(browserSync.stream({ once: true }));
 }
 
 function stylesMin() {
-	return (
-		src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: false })
-			.pipe(
-				sass({
-					outputStyle: 'compressed', // expanded/compressed
-					silenceDeprecations: ['legacy-js-api'], // Disable old API warnings
-				}).on('error', sass.logError),
-			)
-			.pipe(
-				autoprefixer({
-					overrideBrowserslist: browserslist,
-				}),
-			)
-			// .pipe(csso())
-			.pipe(dest(cfg.outputDir + 'css'))
-	);
+	return src(cfg.srcDir + '/scss/**/*.{scss,sass}', { sourcemaps: false })
+		.pipe(
+			sass({
+				outputStyle: 'compressed',
+				silenceDeprecations: ['legacy-js-api'],
+			}).on('error', sass.logError),
+		)
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: browserslist,
+			}),
+		)
+		// .pipe(csso()) // можна розкоментити, якщо хочеш додатково мінімізувати css
+		.pipe(dest(cfg.outputDir + '/css'));
 }
 
 function scripts() {
-	return src(cfg.srcDir + 'js/**/*.js')
+	return src(cfg.srcDir + '/js/**/*.js')
 		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
 		.pipe(concat('script.min.js'))
 		.pipe(terser())
-		.pipe(dest(cfg.outputDir + 'js'))
+		.pipe(dest(cfg.outputDir + '/js'))
 		.pipe(browserSync.stream({ once: true }));
 }
 
 function imageSync() {
-	return src('src/imgs/**/*', { encoding: false })
-		.pipe(dest('app/imgs'))
+	return src(cfg.srcDir + '/imgs/**/*', { encoding: false })
+		.pipe(dest(cfg.outputDir + '/imgs'))
 		.pipe(browserSync.stream({ once: true }));
 }
 
@@ -110,7 +107,7 @@ async function imageSyncMin() {
 	const imageminMozjpeg = (await import('imagemin-mozjpeg')).default;
 	const imageminSvgo = (await import('imagemin-svgo')).default;
 
-	return src('src/imgs/**/*', { encoding: false })
+	return src(cfg.srcDir + '/imgs/**/*', { encoding: false })
 		.pipe(
 			imagemin([
 				imageminPngquant({ quality: [0.6, 0.8], speed: 1 }),
@@ -118,7 +115,7 @@ async function imageSyncMin() {
 				imageminSvgo(),
 			]),
 		)
-		.pipe(dest('app/imgs'))
+		.pipe(dest(cfg.outputDir + '/imgs'))
 		.pipe(browserSync.stream({ once: true }));
 }
 
@@ -131,10 +128,10 @@ function browsersync() {
 }
 
 function watching() {
-	watch([cfg.srcDir + 'scss/**/*.scss'], styles);
-	watch([cfg.srcDir + 'js/**/*.js'], scripts);
-	watch([cfg.srcDir + '/**/*.html'], html);
-	watch([cfg.srcDir + 'imgs/**/*'], imageSync);
+	watch(cfg.srcDir + '/scss/**/*.scss', styles);
+	watch(cfg.srcDir + '/js/**/*.js', scripts);
+	watch(cfg.srcDir + '/**/*.html', html);
+	watch(cfg.srcDir + '/imgs/**/*', imageSync);
 }
 
 async function clean() {
@@ -149,16 +146,16 @@ async function loadPrettier() {
 
 async function pretty() {
 	const prettier = await loadPrettier();
-	return src(['src/**/*', '!src/imgs/**/*']).pipe(prettier()).pipe(dest('src'));
+	return src([cfg.srcDir + '/**/*', '!' + cfg.srcDir + '/imgs/**/*']).pipe(prettier()).pipe(dest(cfg.srcDir));
 }
 
 exports.build = series(
-	clean, // clean the folder
-	htmlMin, // minified HTML
-	fonts, // convert fonts to woff2
-	stylesMin, // minified styles
-	scripts, // minified scripts
-	imageSyncMin, // minified images
+	clean,
+	htmlMin,
+	fonts,
+	stylesMin,
+	scripts,
+	imageSyncMin,
 );
 exports.format = pretty;
 exports.cssmin = stylesMin;
